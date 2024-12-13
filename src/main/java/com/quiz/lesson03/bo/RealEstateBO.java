3b46f97c5d11e5824869c281711981c572198708
DESKTOP-0O2DKLO\comet <gukkyu.comet@gmail.com>
2024-12-13 21:24:45 +0900

    수수정

--- src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
+++ src/main/java/com/quiz/lesson03/bo/RealEstateBO.java
@@ -3,10 +3,12 @@ package com.quiz.lesson03.bo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.quiz.lesson03.domain.RealEstate;
 import com.quiz.lesson03.mapper.RealEstateMapper;
 
+@Service
 public class RealEstateBO {
 
 	@Autowired
--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -2,10 +2,12 @@ package com.quiz.lesson03.mapper;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.quiz.lesson03.domain.RealEstate;
 
+@Mapper
 public interface RealEstateMapper {
 	public List<RealEstate> selectRealEstateByAreaPrice(@Param("area") int area, @Param("price") int price);
 }
