0cf09fcb4673d7be527152008251b2e8c9fc771a
DESKTOP-0O2DKLO\comet <gukkyu.comet@gmail.com>
2024-12-13 20:53:56 +0900

    20241213 수정

--- src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
+++ src/main/java/com/quiz/lesson03/mapper/RealEstateMapper.java
@@ -2,16 +2,10 @@ package com.quiz.lesson03.mapper;
 
 import java.util.List;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import com.quiz.lesson03.domain.RealEstate;
 
-@Mapper
 public interface RealEstateMapper {
-
-	public RealEstate selectRealEstateById(int id);
-	public List<RealEstate> selectRealEstateByRentPrice(Integer rentPrice);
 	public List<RealEstate> selectRealEstateByAreaPrice(@Param("area") int area, @Param("price") int price);
-	public List<RealEstate> selectRealEstate(@Param("area") int area, @Param("price") int price);
 }
